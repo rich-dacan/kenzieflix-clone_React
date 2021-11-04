267bafbeda3f3fe302d79652315cac1aa4812044
Richard Cândido <rich_dacan@protonmail.com>
2021-11-04 16:41:00 -0300

    Update Parte 1
    Nesse ponto, já solicitamos todas as informações necessárias e começaremos a estruturar o projeto!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 
 export default () => {
+
+    const [movieList, setMovieList] = useState([]); // ISSO É REACT PURO!!!
 
     useEffect(() => {
         const loadAll = async () => {
             // PEGANDO A LISTA TOTAL
             let list = await Tmdb.getHomeList();
-            console.log(list);
+            setMovieList(list);
         }
 
         loadAll();
