f2057e00de79af8aaf150bf242cb65269615fada
Richard Cândido <rich_dacan@protonmail.com>
2021-11-05 15:40:21 -0300

    Update
    Começo do carrossel!

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,11 +1,19 @@
 import React from "react";
 import './MovieRow.css';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'; //Agora foi!!
+
 
 export default ({title, items})=>{
     return (
         <div className="movieRow"> 
             <h2>{title}</h2>
-
+            <div className="movieRow--left">
+                <NavigateBeforeIcon style={{fontSize: 50}} />
+            </div>
+            <div className="movieRow--right">
+                <NavigateNextIcon style={{fontSize: 50}} />
+            </div>
             <div className="movieRow--listarea">
 
                 <div className="movieRow--list"> 
