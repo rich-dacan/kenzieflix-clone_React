0425893727dffc37f03fd32d3c7641bcb538cb98
Richard Cândido <rich_dacan@protonmail.com>
2021-11-05 07:47:39 -0300

    Up

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ import FeaturedMovie from './components/FeaturedMovie';
 export default () => {
 
     const [movieList, setMovieList] = useState([]); // ISSO É REACT PURO!!!
+    const [featuredData, setFeaturedData] = useState([null]);
 
     useEffect(() => {
         const loadAll = async () => {
             // PEGANDO A LISTA TOTAL
             let list = await Tmdb.getHomeList();
             setMovieList(list);
+
+            //PEGANDO O FEATURED
+            let originals = list.filter(i=>i.slug === 'originals'); //FILTRANDO FILMES DE APENAS UMA LISTA, NESTE CASO ORIGINALS
+
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1 )); //GERANDO ALEATORIEDADE PRA ESCOLHER UM FILME EM DESTAQUE
+
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+            setFeaturedData(chosenInfo);
         }
 
         loadAll();
@@ -22,7 +33,9 @@ export default () => {
             
         <div className="page">
 
-
+            {featuredData &&
+                <FeaturedMovie item={featuredData} />
+            }
             
             <section className="lists">
                 {movieList.map((item, key)=>(
